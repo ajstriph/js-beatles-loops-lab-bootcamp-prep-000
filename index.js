01bc57fd011e5a6f91e2ac47428c67e6aec18a8d
Andy Striph <ajstriph@me.com>
2017-10-11 17:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var theBeatlesPlay = function (musicians, instruments){
   var beatles = [];
+  i++;
   for (let i = 0; i < 4; i++){
     beatles = `${musicians[i]} plays the ${instruments[i]}`;
   }
