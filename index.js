be72d83ae1c32cd59b95d74f528b94c4fdd1a21a
Andy Striph <ajstriph@me.com>
2017-10-11 18:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function iLoveTheBeatles (number){
   var array = [];
   number++;
   do {
-    array.push("I love the Beatles!");
+    array.push(`I love the Beatles!`);
   }
   while (number <= 7);
   return array;
